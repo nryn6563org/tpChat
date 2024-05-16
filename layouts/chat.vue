94eb99c559290d366e76d876fc5321c8c6a310bc
jhLee <kkndw2659@kakao.com>
2024-05-16 14:44:49 +0900

    update

--- layouts/chat.vue
+++ layouts/chat.vue
@@ -14,28 +14,6 @@ export default {
   components: {
     ChatHeader,
     ChatFooter
-  },
-  mounted() {
-    const inputs = document.querySelectorAll('input')
-    inputs.forEach((input) => {
-      input.addEventListener('focus', this.disableScroll)
-      input.addEventListener('blur', this.enableScroll)
-    })
-  },
-  beforeDestroy() {
-    const inputs = document.querySelectorAll('input')
-    inputs.forEach((input) => {
-      input.removeEventListener('focus', this.disableScroll)
-      input.removeEventListener('blur', this.enableScroll)
-    })
-  },
-  methods: {
-    disableScroll() {
-      document.body.style.overflow = 'hidden'
-    },
-    enableScroll() {
-      document.body.style.overflow = ''
-    }
   }
 }
 </script>
