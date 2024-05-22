01111471c3a8493e31e739f5537cb67191881a19
jhLee <77824683+nryn6563org@users.noreply.github.com>
2024-05-22 09:51:42 +0900

    head update

--- layouts/chat.vue
+++ layouts/chat.vue
@@ -14,9 +14,32 @@ export default {
   components: {
     ChatHeader,
     ChatFooter
+  },
+  head() {
+    if (process.client) {
+      const viewportWidth = window.innerWidth
+      let newContent = ''
+
+      if (viewportWidth > 600) {
+        newContent = 'width=device-width, initial-scale=1.0, user-scalable=no'
+      } else if (viewportWidth <= 600) {
+        newContent = 'width=360px'
+      }
+
+      return {
+        meta: [
+          {
+            name: 'viewport',
+            content: newContent
+          }
+        ]
+      }
+    }
+    return {}
   }
 }
 </script>
+
 <style scoped>
 @import '~/assets/css/offcanvas.css';
 @import '~/assets/css/button.css';
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,22 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=360px' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'viewport', content: 'width=360px' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'text/css', href: 'https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' },
-      { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css'
+      },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+      },
       {
         rel: 'stylesheet',
         href: 'https://fastly.jsdelivr.net/npm/swiper@10.2.0/swiper-bundle.min.css'
@@ -29,9 +37,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/css/common.css'
-  ],
+  css: ['~/assets/css/common.css'],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
@@ -39,21 +45,17 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/eslint-module'
-  ],
+  buildModules: ['@nuxtjs/eslint-module'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 
   target: 'static',
 
   router: {
     base: '/chatbot/'
   }
-
 }
