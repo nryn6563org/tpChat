efb6acc254847791c09b031b8d964579281f3e99
jhLee <77824683+nryn6563org@users.noreply.github.com>
2024-05-22 14:33:11 +0900

    2b0dced

---
+++ _nuxt/static/1715927189/chating/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fchatbot\u002F_nuxt\u002Fstatic\u002F1715927189",layout:"chat",error:a,serverRendered:true,routePath:"\u002Fchating",config:{_app:{basePath:"\u002Fchatbot\u002F",assetsPath:"\u002Fchatbot\u002F_nuxt\u002F",cdnURL:a}}}}(null));
